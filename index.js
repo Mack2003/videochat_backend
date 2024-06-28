f6d87cadbf5b3f2880cd95af43cdd639a3a4e6b4
Mack2003 <moloyroy8906144923@gmail.com>
2024-06-28 16:06:06 +0530

    serverActive

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
-const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
 require('dotenv').config();
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
 
-app.listen(process.env.PORT, err => err? console.log(err) : console.log(`Server is running on port number ${process.env.PORT}`));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  // Example of receiving a message from client
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('message', msg); // Broadcast message to all clients
+  });
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
