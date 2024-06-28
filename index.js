207373689c481cb3ec7255630117b8c44c6ac671
Mack2003 <moloyroy8906144923@gmail.com>
2024-06-28 13:34:18 +0530

    starting server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 
+app.listen(process.env.PORT, err => err? console.log(err) : console.log(`Server is running on port number ${process.env.PORT}`));
